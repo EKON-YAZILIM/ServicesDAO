a1b018d743790c46ae04e1d59f2e49d9a4deec9d
ebu060 <ebubekir.yrd@gmail.com>
2021-10-13 18:18:59 +0300

    Home pages edited. Job card design started.

--- DAO_WebPortal/Controllers/HomeController.cs
+++ DAO_WebPortal/Controllers/HomeController.cs
@@ -18,48 +18,24 @@ namespace DAO_WebPortal.Controllers
         {
             return View();
         }
-        [Route("Jobs")]
-        public IActionResult Jobs()
+        [Route("My-Jobs")]
+        public IActionResult My_Jobs()
         {
             return View();
         }
-        [Route("New-Job")]
-        public IActionResult New_Job()
+        [Route("All-Jobs")]
+        public IActionResult All_Jobs()
         {
             return View();
         }
-        [Route("Forum")]
-        public IActionResult Forum()
-        {
-            return View();
-        }
-        [Route("Forum-Detail")]
-        public IActionResult Forum_Detail()
-        {
-            return View();
-        }
+
         [Route("Auctions")]
         public IActionResult Auctions()
         {
             return View();
         }
-        [Route("Auction-Detail")]
-        public IActionResult Auction_Detail()
-        {
-            return View();
-        }
-        [Route("Voting")]
-        public IActionResult Voting()
-        {
-            return View();
-        }
-        [Route("Voting-Detail")]
-        public IActionResult Voting_Detail()
-        {
-            return View();
-        }
-        [Route("User-Profile")]
-        public IActionResult User_Profile()
+        [Route("Votes")]
+        public IActionResult Votes()
         {
             return View();
         }
@@ -70,6 +46,11 @@ namespace DAO_WebPortal.Controllers
         }
         [Route("Payments-History")]
         public IActionResult Payments_History()
+        {
+            return View();
+        }
+        [Route("User-Profile")]
+        public IActionResult User_Profile()
         {
             return View();
         }
