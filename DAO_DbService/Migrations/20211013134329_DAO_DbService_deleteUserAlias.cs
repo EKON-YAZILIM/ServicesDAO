ed0d8b17206518585500598e7d33299b5dca8ec9
yalcinyasin14@gmail.com <yalcinyasin14@gmail.com>
2021-10-13 16:46:28 +0300

    Update User Model and Dto

---
+++ DAO_DbService/Migrations/20211013134329_DAO_DbService_deleteUserAlias.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DAO_DbService.Migrations
+{
+    public partial class DAO_DbService_deleteUserAlias : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserAlias",
+                table: "Users");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserAlias",
+                table: "Users",
+                type: "text",
+                nullable: true);
+        }
+    }
+}
