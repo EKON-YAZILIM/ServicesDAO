919a0b596f5375e0360633d451831b8e5938d84e
tcitaylan <taylancivaoglu@gmail.com>
2021-12-19 21:20:05 +0300

    IdentitiyService_Tests_Updated && appsett

--- UnitTests/DAO_IdentityService.Test/DAL.cs
+++ UnitTests/DAO_IdentityService.Test/DAL.cs
@@ -8,26 +8,7 @@ namespace DAO_IdentityService.Test
     public class settings{
         public string DbConnectionString { get; set; }
     }
-    public interface IRabbitMQConnectionFactory {
-        IConnection CreateConnection();
-    }
-    // public class RabbitMQConnection : IRabbitMQConnectionFactory {
-    //     private readonly RabbitMQConnectionDetail connectionDetails;
-        
-    //     public RabbitMQConnection(IOptions<RabbitMQConnectionDetail> connectionDetails) {
-    //         this.connectionDetails = connectionDetails.Value;
-    //     }
-
-    //     public IConnection CreateConnection() {
-    //         var factory = new ConnectionFactory {
-    //             HostName = connectionDetails.HostName,
-    //             UserName = connectionDetails.UserName,
-    //             Password = connectionDetails.Password
-    //         };
-    //         var connection = factory.CreateConnection();
-    //         return connection;
-    //     }
-    // }
+    
     public static class DAL{
         public static string connectionString = "";
         
@@ -36,13 +17,12 @@ namespace DAO_IdentityService.Test
 
         static DAL(){
             _settings = new settings();
-            var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            var config = new ConfigurationBuilder().AddJsonFile("appsettings.test.json").Build();
             var cs = config.GetSection("PlatformSettings");
             cs.Bind(_settings);            
         }
 
-        static bool ExecuteCommand(string command){
-            
+        static bool ExecuteCommand(string command){            
             using (conn = new MySqlConnection(_settings.DbConnectionString)){
                 MySqlCommand comm = new MySqlCommand(command, conn);
                 conn.Open();
@@ -51,26 +31,32 @@ namespace DAO_IdentityService.Test
             }         
         }
 
+        ///Email activation 
         public static bool ActivateUser(string Email){
-            bool result = false;
-            string commandString = "";
-            using(conn = new MySqlConnection(_settings.DbConnectionString)){
-                MySqlCommand comm = new MySqlCommand(commandString,conn);
-                conn.Open();
-
-
-            }
-            return result;
+            return ExecuteCommand("update Users set IsActive = 1 where Email = '" + Email + "'");            
         }
 
-        public static bool ActivateKYC(string email){
-            bool result = false;
-            return result;
+        ///KYC verification
+        public static bool ActivateKYC(string Email){
+            return ExecuteCommand("update Users set KYCStatus = 1 where Email = '" + Email + "'");
         }
 
+        ///Truncating Users table 
         public static bool ClearUsersTable(){
             return ExecuteCommand("truncate table Users");            
         }
+
+        ///Blocks user
+        public static bool BlockUser(string Email){
+            return ExecuteCommand("update Users set IsActive = 1, KYCStatus = 1, IsBlocked = 1 where Email = '" + Email + "'");            
+        }
+
+        ///Resets user to initial register state
+        public static bool ResetUser(string Email){
+            return ExecuteCommand("update Users set IsActive = 0, KYCStatus = 0, IsBlocked = 0 where Email = '" + Email + "'");
+        }
+
+
     }
     
 }
